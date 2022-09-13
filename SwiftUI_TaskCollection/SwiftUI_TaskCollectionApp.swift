d8f5a964970d6cd07500ea6b23558bf410952209
Aleksei Lokhin <lokhin.a.a@gmail.com>
2022-09-13 10:01:30 +0300

    Initial Commit

---
+++ SwiftUI_TaskCollection/SwiftUI_TaskCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TaskCollectionApp.swift
+//  SwiftUI_TaskCollection
+//
+//  Created by Алексей on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TaskCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
